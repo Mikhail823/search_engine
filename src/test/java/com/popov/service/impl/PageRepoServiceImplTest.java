12a189fdc8300700279690cea9166feec8c63337
Mikhail823 <rabota822@bk.ru>
2022-10-17 22:47:18 +0300

    testings1

--- src/test/java/com/popov/service/impl/PageRepoServiceImplTest.java
+++ src/test/java/com/popov/service/impl/PageRepoServiceImplTest.java
@@ -2,39 +2,66 @@ package com.popov.service.impl;
 
 import com.popov.models.Page;
 import com.popov.repo.PageRepository;
-import com.popov.service.PageRepositoryService;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
-@SpringBootTest
-class PageRepoServiceImplTest {
 
-    private String URL = "http://www.playback.ru";
+
+@DataJdbcTest
+class PageRepoServiceImplTest {
     @Autowired
-    private PageRepository pageRepository;
+    private PageRepository repository;
     @Autowired
-    private PageRepositoryService pageRepositoryService;
+    private TestEntityManager entityManager;
+
+    private Page p1, p2;
+    @BeforeEach
+    void setUp(ApplicationContext context) {
+        //Создание тестовой страницы
+        p1 = new Page();
+        p1.setId(1);
+        p1.setCode(200);
+        p1.setPath("/ru.html");
+        p1.setContent("<html><title>Hellow Word!!</title></html>");
+
+        p2 = new Page();
+        p2.setId(2);
+        p2.setCode(200);
+        p2.setPath("/rukk.html");
+        p2.setContent("<html><title>Hellow Word!! Puss</title></html>");
+
+        repository.saveAll(List.of(p1, p2));
+    }
+
+    @AfterEach
+    void tearDown() {
+        repository.deleteAll();
+    }
+
 
     @Test
     void getPage() {
-        Page page = new Page();
-        page.setPath("/error.html");
-        page.setSiteId(200);
-        page.setContent("<body>jsbfhydsbfhybshdb</body>");
-        pageRepository.save(page);
-        pageRepository.findByPath("/error.html");
-
+        assertEquals(repository.findByPath("/rukk.html"), "/rukk.html");
     }
 
     @Test
     void save() {
-        Page page = new Page();
-        page.setPath("/content.html");
-        page.setCode(200);
-        page.setContent("<html></html>");
-        pageRepository.save(page);
+
     }
 
     @Test
@@ -42,18 +69,11 @@ class PageRepoServiceImplTest {
     }
 
     @Test
-    void findPageByPageIdAndSiteId() {
-    }
-
-    @Test
-    void pageCount() {
-    }
-
-    @Test
-    void testPageCount() {
-    }
-
-    @Test
     void deletePage() {
+    }
+
+    @Test
+    void testFindAll(){
+        assertEquals(List.of(p1, p2), repository.findAll());
     }
 }
--- src/test/java/com/popov/sitemap/ParseUrlTest.java
+++ src/test/java/com/popov/sitemap/ParseUrlTest.java
@@ -6,23 +6,25 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+import org.mockito.stubbing.Answer;
 
+import java.io.File;
 import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ParseUrlTest {
-    private String url = "C:\\Users\\User\\IdeaProjects\\SkilBox_diplom\\search_engine\\src\\main\\resources\\templates\\test_page.html";
+    private String url = "src/main/resources/templates/test_page.html";
+
+    @Test
+    void computeTestInterrupted() {
+        ParseUrl parseUrl = new ParseUrl("https://www.playback.ru", true);
+        assertEquals("", parseUrl.compute());
+    }
+
     @Test
     void compute() throws IOException, InterruptedException {
-
         Document document = Jsoup.parse(url);
-        String URL = "http://www.playback.ru";
-
-        ParseUrl parseUrl = new ParseUrl(URL, false);
-        Mockito.doReturn(document).when(Mockito.mock(ParseUrl.class).getDocumentByUrl(url));
-
-        MatcherAssert.assertThat(parseUrl.compute(), Matchers.containsString("dsfdfafdsf"));
     }
 
     @Test
